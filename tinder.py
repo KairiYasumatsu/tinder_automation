2953d30148d4a112b67f0432f359d1e4d737317f
kairi Yasumatsu <kairi.matibuse@gmail.com>
2020-07-12 11:42:39 +0900

    firstメッセージ自動化/class内のモジュール化

--- tinder.py
+++ tinder.py
@@ -1,8 +1,9 @@
 # coding:utf-8
+import emoji
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from time import sleep
+from time import process_time, sleep
 import chromedriver_binary
 import settings
 
@@ -14,76 +15,116 @@ fbLoginXpath = "/html/body/div/div[2]/div[1]/form/div/div[3]/label[2]/input"
 FbMail = settings.FacebookMail
 FbPass = settings.FacebookPass
 
-# ブラウザを開く。
-driver = webdriver.Chrome()
+class tinder:
+    def __init__(self):
+        # ブラウザを開く。
+        self.driver = webdriver.Chrome()
 
-# facebookアクセストークンを開く。
-driver.get(fbAuthPass)
+    def login(self):
+        # facebookアクセストークンを開く。
+        self.driver.get(fbAuthPass)
 
-#　メール入力
-mailField = driver.find_element_by_xpath(fbMailXpath)
-mailField.send_keys(FbMail)
+        #　メール入力
+        mailField = self.driver.find_element_by_xpath(fbMailXpath)
+        mailField.send_keys(FbMail)
 
-#　パスワード入力
-passFirld = driver.find_element_by_xpath(fbPassXpath)
-passFirld.send_keys(FbPass)
-print("アカウント情報入力完了")
+        #　パスワード入力
+        passFirld = self.driver.find_element_by_xpath(fbPassXpath)
+        passFirld.send_keys(FbPass)
+        print("アカウント情報入力完了")
 
-#　ログイン押下s
-loginField = driver.find_element_by_xpath(fbLoginXpath)
-loginField.click()
-print("ログイン成功")
+        #　ログイン押下s
+        loginField = self.driver.find_element_by_xpath(fbLoginXpath)
+        loginField.click()
+        print("ログイン成功")
 
-# tinderトップページ遷移
-driver.execute_script("window.open()")
-new_window = driver.window_handles[1]
-driver.switch_to.window(new_window)
-driver.get("https://tinder.com/app")
-print("トップページ遷移完了")
+        # tinderトップページ遷移
+        self.driver.execute_script("window.open()")
+        new_window = self.driver.window_handles[1]
+        self.driver.switch_to.window(new_window)
+        self.driver.get("https://tinder.com/app")
+        print("トップページ遷移完了")
 
-# モーダル表示待ち
-sleep(5)
-# アカウントfacebookログイン
-fbButton = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div/div[3]/span/div[2]/button")
-fbButton.click()
+        # モーダル表示待ち
+        sleep(5)
+        # アカウントfacebookログイン
+        fbButton = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div/div[3]/span/div[2]/button")
+        fbButton.click()
 
-driver.implicitly_wait(10)
-notificationButtonXpath = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]")
-notificationButtonXpath.click()
-print("位置情報on")
+        self.driver.implicitly_wait(10)
+        notificationButtonXpath = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]")
+        notificationButtonXpath.click()
+        print("位置情報on")
 
-driver.implicitly_wait(10)
-notificationButtonXpath = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]")
-notificationButtonXpath.click()
-print("アラート表記on")
+        self.driver.implicitly_wait(10)
+        notificationButtonXpath = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]")
+        notificationButtonXpath.click()
+        print("アラート表記on")
 
-driver.implicitly_wait(10)
-cookieAgreeButtonXpath = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div[1]/button")
-cookieAgreeButtonXpath.click()
-print("cookie同意")
+        self.driver.implicitly_wait(10)
+        cookieAgreeButtonXpath = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div[1]/button")
+        cookieAgreeButtonXpath.click()
+        print("cookie同意")
 
-#  写真のロードまで待つ
-driver.implicitly_wait(10)
-targetImage = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/div[1]/div/div") 
-i = 0
-while targetImage:
-    driver.implicitly_wait(5)
-    # 右スワイプ
-    try:
-        likeButton = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button")
-        likeButton.click()
-        i += 1
-        print(i, "件いいね完了")
-        driver.implicitly_wait(5)
-        targetImage = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/div[1]/div/div") 
-        continue
+    def autoSwipe(self, login):
+        login()
+        #  写真のロードまで待つ
+        self.driver.implicitly_wait(10)
+        targetImage = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/div[1]/div/div") 
+        i = 0
+        while targetImage:
+            self.driver.implicitly_wait(5)
+            # 右スワイプ
+            try:
+                likeButton = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button")
+                likeButton.click()
+                i += 1
+                print(i, "件いいね完了")
+                self.driver.implicitly_wait(5)
+                targetImage = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/div[1]/div/div") 
+                continue
 
-    except Exception as e:
-        try:
-            addHomeButton = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[2]/button[2]")
-            addHomeButton.click()
-            print("例外モーダルを処理")
-            continue
+            except Exception as e:
+                try:
+                    addHomeButton = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div[2]/button[2]")
+                    addHomeButton.click()
+                    print("例外モーダルを処理")
+                    continue
 
-        except Exception as e:
-            driver.close()
+                except Exception as e:
+                    self.driver.close()
+
+    def autoMessage(self, login):
+        login()
+        i = 2
+
+        # 直近のマッチを50件取得
+        while i < 50:
+            # 次のマッチを取得
+            matchedXpath = "/html/body/div[1]/div/div[1]/div/aside/nav/div/div/div/div[2]/div[1]/div[1]/div[" + str(i) + "]/a"
+            self.driver.implicitly_wait(10)
+            getMatched = self.driver.find_element_by_xpath(matchedXpath)
+            getMatched.click()
+            
+            # メッセージセット　送信
+            self.driver.implicitly_wait(10)
+            getTextArea = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div[1]/div/div/div[3]/form/textarea")
+            emoji = chr(int(0x1f618))
+
+            # seleniumで絵文字が動かなかったので、Javascriptで対応
+            INPUT_EMOJI = """
+            arguments[0].value += arguments[1];
+            arguments[0].dispatchEvent(new Event('input'));
+            """
+            text = "お、女神現れた" + emoji
+            self.driver.execute_script(INPUT_EMOJI, getTextArea, text)
+            # 明示的にスペースを入力して送信ボタンを有効にする
+            getTextArea.send_keys(" ")
+            print("メッセージセット完了")
+
+            self.driver.implicitly_wait(10)
+            getSendButton = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div[1]/div/div/div[3]/form/button")
+            getSendButton.click()
+            print("メッセージ送信完了")
+
+            i += 1
